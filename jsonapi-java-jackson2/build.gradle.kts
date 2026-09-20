fc7b4ff4ee4fed5f64765b8d8216324f62398cd9
[bot] ck-reviewer <christopher.kazemek+reviewer@googlemail.com>
2026-09-20 10:55:08 +0200

    fix: add JSpecify to Jackson prototype tests

--- jsonapi-java-jackson2/build.gradle.kts
+++ jsonapi-java-jackson2/build.gradle.kts
@@ -16,4 +16,5 @@ dependencies {
     testImplementation(testFixtures(project(":jsonapi-java-mapping")))
     testImplementation(libs.archunit)
     testImplementation(libs.json.schema.validator)
+    testCompileOnly(libs.jspecify)
 }
